176448799486369f32f25b2c9cd575e370a7ee81
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-12-27 15:43:49 +0100

    Added wrapper div

--- templates/timeline.blade.php
+++ templates/timeline.blade.php
@@ -2,31 +2,36 @@
     @if (!$hideTitle && !empty($post_title))
     <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
     @endif
-    <ul class="timeline">
-    	@foreach($events as $event)
-	        <li class="timeline-event">
-				<div class="timeline-date hidden-xs hidden-sm">
-	            	{!! \ModularityTimeline\Module::timelineDate($ID, $event['date']) !!}
-				</div>
-	            <div class="timeline-marker">
-	            	<div class="timeline-date hidden-md hidden-lg">
-		            	{!! \ModularityTimeline\Module::timelineDate($ID, $event['date']) !!}
-					</div>
-	            </div>
-	            <div class="timeline-content timeline-{{ $event['image_position'] }}">
-					<div class="grid">
-						@if(!empty($event['image']['ID']))
-							<div class="{{ $event['image_grid'] }}">
-								{!! $event['image_markup'] !!}
-	                		</div>
-	                	@endif
-						<div class="{{ $event['content_grid'] }}">
-							<h3>{{ $event['title'] }}</h3>
-	                		{!! $event['content'] !!}
-	                	</div>
-					</div>
-	            </div>
-	        </li>
-        @endforeach
-    </ul>
+    <div class="timeline-box">
+        <ul class="timeline">
+            @foreach($events as $event)
+                <li class="timeline-event">
+                    <div class="timeline-date hidden-xs hidden-sm">
+                        {!! \ModularityTimeline\Module::timelineDate($ID, $event['date']) !!}
+                    </div>
+                    <div class="timeline-marker">
+                        <div class="timeline-date hidden-md hidden-lg">
+                            {!! \ModularityTimeline\Module::timelineDate($ID, $event['date']) !!}
+                        </div>
+                    </div>
+                    <div class="timeline-content timeline-{{ $event['image_position'] }}">
+                        <div class="grid">
+                            @if(!empty($event['image']['ID']))
+                                <div class="{{ $event['image_grid'] }}">
+                                    {!! $event['image_markup'] !!}
+                                </div>
+                            @endif
+                            <div class="{{ $event['content_grid'] }}">
+                                <div class="timeline-date">
+                                    {!! \ModularityTimeline\Module::timelineDate($ID, $event['date']) !!}
+                                </div>
+                                <h3 class="timeline-title">{{ $event['title'] }}</h3>
+                                {!! $event['content'] !!}
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
